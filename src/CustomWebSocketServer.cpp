d510204c6510545ba9575ba45b880b904225eee0
YueYuuru <yueyuuru@gmail.com>
2025-10-16 00:57:56 +0800

    Update: 將自訂 WebSocket 伺服器程式碼從 SmartTrashCan.ino 遷移到 CustomWebSocketServer.cpp

--- src/CustomWebSocketServer.cpp
+++ src/CustomWebSocketServer.cpp
@@ -1,11 +1,24 @@
 #include <Arduino.h>
+#include <vector>
+#include <WebSocketsServer.h>
 
 
 #include "CustomWebSocketServer.h"
 
 
-CustomWebSocketServer::CustomWebSocketServer() {
+// 儲存 client_id 的容器
+std::vector<uint8_t> clients;
 
+// WebSocket 伺服器實例
+WebSocketsServer webSocket = WebSocketsServer(81);    // Port 81
+
+
+void webSocketEvent(uint8_t client_id, WStype_t event_type, uint8_t *payload, size_t length);
+void onReceivedMessage(String message);
+
+
+CustomWebSocketServer::CustomWebSocketServer() {
+	
 }
 
 CustomWebSocketServer& CustomWebSocketServer::getInstance() {
@@ -16,10 +29,79 @@ CustomWebSocketServer& CustomWebSocketServer::getInstance() {
 CustomWebSocketServer& customWebSocketServer = CustomWebSocketServer::getInstance();    // WebSocket 伺服器
 
 void CustomWebSocketServer::setup() {
+	// 啟動 WebSocket 伺服器
+	webSocket.begin();
 
+	// 伺服器事件
+	webSocket.onEvent(webSocketEvent);
 }
 
 
 void CustomWebSocketServer::loop() {
-    
+    // 每個循環要處理 WebSocket 的事件
+	webSocket.loop();
 }
+
+
+// 當有 WebSocket 資料進來時呼叫
+void webSocketEvent(uint8_t client_id, WStype_t event_type, uint8_t *payload, size_t length) {
+	switch (event_type) {
+		case WStype_CONNECTED: {
+			clients.push_back(client_id);
+			Serial.printf("Client [%u] connected", client_id);
+			Serial.println();
+			IPAddress ip = webSocket.remoteIP(client_id);
+			Serial.print("Client IP: ");
+			Serial.println(ip);
+
+			webSocket.sendTXT(client_id, "Hello from ESP32 Websocket Server! # 你已經連線 這是一條測試訊息 來自ESP32");
+
+			break;
+		}
+		case WStype_TEXT: {
+			String message = String((char*)payload);
+			Serial.printf("Received from client [%u]: %s", client_id, message.c_str());
+			Serial.println();
+
+			// Echo message back
+			// webSocket.sendTXT(num, "ESP received: " + message);
+
+			onReceivedMessage(message);
+
+			break;
+		}
+		case WStype_DISCONNECTED: {
+			clients.erase(std::remove(clients.begin(), clients.end(), client_id), clients.end());
+			Serial.printf("Client [%u] disconnected", client_id);
+			Serial.println();
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+}
+
+
+void onReceivedMessage(String message) {
+	
+	/*
+	if (message == "App.TrashCanActivity.switch_led.on") {
+		trashCanLight.setEnable(true);
+	}
+
+	if (message == "App.TrashCanActivity.switch_led.off") {
+		trashCanLight.setEnable(false);
+	}
+
+	if (message == "App.TrashCanActivity.switch_cover.on") {
+		trashCanLid.setOpen(true);
+	}
+
+	if (message == "App.TrashCanActivity.switch_cover.off") {
+		trashCanLid.setOpen(false);
+	}
+	*/
+	
+}
+
--- src/SmartTrashCan.ino
+++ src/SmartTrashCan.ino
@@ -14,12 +14,13 @@
 
 
 
-#include "WifiConnect.h"            // Wifi
-#include "DatabaseClient.h"         // 資料庫
-#include "TrashCanLight.h"          // 垃圾桶燈
-#include "TrashCanLid.h"            // 垃圾桶蓋
-#include "TrashCanCapacity.h"       // 垃圾桶容量
-#include "EnvironmentalSensor.h"    // 環境感應器
+#include "WifiConnect.h"              // Wifi
+#include "CustomWebSocketServer.h"    // 自訂 WebSocketServer
+#include "DatabaseClient.h"           // 資料庫
+#include "TrashCanLight.h"            // 垃圾桶燈
+#include "TrashCanLid.h"              // 垃圾桶蓋
+#include "TrashCanCapacity.h"         // 垃圾桶容量
+#include "EnvironmentalSensor.h"      // 環境感應器
 
 
 // 任務
@@ -30,14 +31,6 @@ TaskHandle_t taskHandle_trashCanCapacity;
 TaskHandle_t taskHandle_environmentalSensor;
 
 
-// 儲存 client_id 的容器
-std::vector<uint8_t> clients;
-
-// 搞伺服器在ESP32上
-WebSocketsServer webSocket = WebSocketsServer(81);  // Port 81
-
-
-
 // WifiConnect& wifiConnect = WifiConnect::getInstance();            // Wifi
 // DatabaseClient& databaseClient = DatabaseClient::getInstance();   // 資料庫
 // TrashCanLight& trashCanLight = TrashCanLight::getInstance();      // 垃圾桶燈
@@ -45,8 +38,7 @@ WebSocketsServer webSocket = WebSocketsServer(81);  // Port 81
 
 RealtimeDatabase database;
 
-void onReceivedMessage(String message);
-void webSocketEvent(uint8_t client_id, WStype_t event_type, uint8_t *payload, size_t length);
+
 void task_databaseClient(void * parameter);
 void task_trashCanLight(void * parameter);
 void task_trashCanLid(void * parameter);
@@ -64,6 +56,9 @@ void setup() {
 	databaseClient.setup();
 	database = databaseClient.getRealtimeDatabase();
 
+	// 自訂 WebSocketServer
+	customWebSocketServer.setup();
+
 	// 垃圾桶燈
 	trashCanLight.setup();
 
@@ -76,12 +71,6 @@ void setup() {
 	// 環境感應器
 	environmentalSensor.setup();
 
-
-	// 啟動 WebSocket 伺服器
-	webSocket.begin();
-
-	// 伺服器事件
-	webSocket.onEvent(webSocketEvent);
 
 	xTaskCreate(
 		// 任務函式
@@ -101,10 +90,9 @@ void setup() {
 
 
 void loop() {
-	// 每個循環要處理 WebSocket 的事件
-	webSocket.loop();
-
 	wifiConnect.loop();
+	customWebSocketServer.loop();
+
 
 	/*
 	String serialMessage = "";
@@ -170,65 +158,4 @@ void task_environmentalSensor(void * parameter) {
 }
 
 
-void onReceivedMessage(String message) {
-	
-	/*
-	if (message == "App.TrashCanActivity.switch_led.on") {
-		trashCanLight.setEnable(true);
-	}
 
-	if (message == "App.TrashCanActivity.switch_led.off") {
-		trashCanLight.setEnable(false);
-	}
-
-	if (message == "App.TrashCanActivity.switch_cover.on") {
-		trashCanLid.setOpen(true);
-	}
-
-	if (message == "App.TrashCanActivity.switch_cover.off") {
-		trashCanLid.setOpen(false);
-	}
-	*/
-	
-}
-
-
-
-// 當有 WebSocket 資料進來時呼叫
-void webSocketEvent(uint8_t client_id, WStype_t event_type, uint8_t *payload, size_t length) {
-	switch (event_type) {
-		case WStype_CONNECTED: {
-			clients.push_back(client_id);
-			Serial.printf("Client [%u] connected", client_id);
-			Serial.println();
-			IPAddress ip = webSocket.remoteIP(client_id);
-			Serial.print("Client IP: ");
-			Serial.println(ip);
-
-			webSocket.sendTXT(client_id, "Hello from ESP32 Websocket Server! # 你已經連線 這是一條測試訊息 來自ESP32");
-
-			break;
-		}
-		case WStype_TEXT: {
-			String message = String((char*)payload);
-			Serial.printf("Received from client [%u]: %s", client_id, message.c_str());
-			Serial.println();
-
-			// Echo message back
-			// webSocket.sendTXT(num, "ESP received: " + message);
-
-			onReceivedMessage(message);
-
-			break;
-		}
-		case WStype_DISCONNECTED: {
-			clients.erase(std::remove(clients.begin(), clients.end(), client_id), clients.end());
-			Serial.printf("Client [%u] disconnected", client_id);
-			Serial.println();
-			break;
-		}
-		default: {
-			break;
-		}
-	}
-}
